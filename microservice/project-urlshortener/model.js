674262bfeccbf74a90ff55efe9a24cc5bbb0c1a4
NoobBaez <haniel_0101@hotmail.com>
2020-11-11 23:24:45 -0400

    Fix untracked

--- microservice/project-urlshortener/model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const urlSchema = mongoose.Schema({
-    original_url: String,
-    short_url: String
-});
-
-module.exports = mongoose.model("Url", urlSchema);
